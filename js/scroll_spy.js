11f746c7905cd579c9fd1e6840cef7c6f175d34b
David Francisco <davidfco.pozo@hotmail.com>
2021-05-06 21:39:35 -0400

    Make multilanguage

--- js/scroll_spy.js
+++ js/scroll_spy.js
@@ -10,14 +10,14 @@ export default function scrollSpy() {
       const id = entry.target.getAttribute("id");
 
       if(entry.isIntersecting){
-        d.querySelector(`h2[data-scroll-spy][href="#${id}"]`).classList.add("focused");
+        d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add("focused");
       } else {
-        d.querySelector(`h2[data-scroll-spy][href="#${id}"]`).classList.remove("focused");
+        d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove("focused");
       }
     });
   }
   const observer = new IntersectionObserver(cb, {
-    threshold: [0.5, 0.75],
+    threshold: [0.7, 0.75],
   });
 
   $sections.forEach((el) =>observer.observe(el));
