6f656963bc8286de8cdfc5e3eaee29cee9db5e4b
David Francisco <davidfco.pozo@hotmail.com>
2021-05-30 14:13:19 -0400

    details

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import mobileMenu from "./mobile-menu.js";
 import theme from "./theme.js";
 import scroll_spy from "./scroll_spy.js";
-import smoothScroll from "./smoothscroll.js";
+import smoothScroll from "./smooth-scroll.js";
 
 
 const d = document,
@@ -10,9 +10,9 @@ w =window;
 d.addEventListener("DOMContentLoaded", ()=>{
   mobileMenu(".hamburger-btn", ".menu-bar a");
   scroll_spy();
+  smoothScroll();
 });
 
-smoothScroll();
 
 
 theme(".theme-btn", "dt-background", "dt-text");
--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,5 +1,7 @@
 const d = document;
 
+export default function smoothscroll() {
 d.querySelectorAll("section[data-scroll-spy]").scrollIntoView({ 
-  behavior: 'smooth',
-});
+    behavior: 'smooth',
+  })
+}
