85e186346dac2591bd9e15e7db7e0a6e68b3eded
David Francisco <davidfco.pozo@hotmail.com>
2022-01-09 22:18:53 -0400

    fixing

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -10,12 +10,12 @@ $sections.forEach(el => el.addEventListener("click", (e)=>{
   if(targetId != "#intro") {
     window.scroll({
         top: d.querySelector(targetId).offsetTop - 40,
-        behavior: "smooth"
+        behavior: "auto"
       })
     } else {
     window.scroll({
         top: d.querySelector(targetId).offsetTop - 98,
-        behavior: "smooth"
+        behavior: "auto"
       })
 
   }
